f75ab5d5a927f6cc8d12ade809ce44d69dac4e11
Dorian <dorian.scheidt@gmail.com>
2014-02-26 13:38:32 -0500

    Update browse to make gridlike

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+$(document).foundation({
+  tooltips: {
+    selector : '.has-tip',
+    additional_inheritable_classes : [],
+    tooltip_class : '.tooltip',
+    touch_close_text: 'tap to close',
+    hover_delay: 5000,
+    disable_for_touch: false,
+    tip_template : function (selector, content) {
+      return '<span data-selector="' + selector + '" class="'
+        + Foundation.libs.tooltips.settings.tooltipClass.substring(1)
+        + '">' + content + '<span class="nub"></span></span>';
+    }
+  }
+});
